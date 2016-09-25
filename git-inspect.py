86e5af10ab8bec6c7a8af6b16ba20479e5db4fd9
yanglikkun <yanglikun@jd.com>
2016-09-25 08:35:39 +0800

    添加首页

--- git-inspect.py
+++ git-inspect.py
@@ -1,12 +1,24 @@
-from flask import Flask
+from flask import Flask, request, render_template
+import logging
+import org.btlas.startWithRepo as startWithRespo
+
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template("index.html")
+
+
+@app.route("/getAllCommit", methods=["POST"])
+def xf():
+    return startWithRespo.getAllCommitJSON(request.form['workingDir'])
 
 
 if __name__ == '__main__':
-    app.run()
+    handler = logging.FileHandler(r'flask.log')
+    handler.setLevel(logging.DEBUG)
+    app.logger.addHandler(handler)
+    app.run(host="0.0.0.0", port=8099,debug=True)
