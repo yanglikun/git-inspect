df69c211714e55dc1db2d4592f76030994085b35
likunyang <yanglikun0@gmail.com>
2016-10-15 17:10:21 +0800

    添加注释

--- org/manual/generateHash.py
+++ org/manual/generateHash.py
@@ -1,13 +1,13 @@
 from hashlib import sha1
 
-fileContent = '789\n'
+#文件内容
+fileContent = '123\n'
+#文件内容长度
 fileContentLength = len(fileContent)
+#blob文件内容
 data = 'blob ' + str(fileContentLength) + "\0" + fileContent
-
+#SHA-1运算
 hashObj = sha1()
-
 hashObj.update(data.encode('utf-8'))
-
 hashValue = hashObj.hexdigest()
-
 print(hashValue)
--- org/manual/parseFile.py
+++ org/manual/parseFile.py
@@ -1,13 +1,16 @@
 import zlib
 
 objDir = "E:/git/course/test/.git/objects";
-prefixDir = "0c"
-fileName = "2b7810b5ff9dc14a44270f1f9c3bae28f74855"
+prefixDir = "19"
+fileName = "0a18037c64c43e6b11489df4bf0b9eb6d2c9bf"
 
+#读取文件
 zlibBinaryData = open(objDir + "/" + prefixDir + "/" + fileName, 'rb').read()
 
+#zlib解压
 binaryData = zlib.decompress(zlibBinaryData);
 
+#输出内容
 print(binaryData.decode('utf-8'))
 
 
