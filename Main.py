435a19369779c8b370b06afa0af0cceb869475e2
yanglikun <yanglikun@jd.com>
2016-11-19 11:18:28 +0800

    修改README说明

--- Main.py
+++
@@ -1,8 +0,0 @@
-__author__='yanglikun'
-columns=' id, plu, sku_id, sku_amount,ticket_type_person,created, modified, yn'
-fields='id,plu,skuId,skuAmount,ticketTypePerson,created,modified,yn'
-
-columnArr=columns.split(",")
-fieldsArr=fields.split(",")
-for a,b in zip(columnArr,fieldsArr):
-    print('<result column="'+a+'"  property="'+b+'"/>')
--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 
 #用于可视化Git仓库的内容，用于深入认识git存储
 #基于python3开发
+##解析文件：org/manual/parseFile.py
+##生成hash：org/manual/generateHash.py
 
 ![说明图片](http://yanglikun.github.io/github/git-inspect.png)
